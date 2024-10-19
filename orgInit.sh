fdb7980a31b387b2a40ea935f24c22f0039979da
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-19 00:22:08 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,18 +1,18 @@
 # Create the demo org
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix flow -o orchestrator.demo
+sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -s -p flow -e orchestrator.demo
 
 # Push the metadata into the new scratch org.
-sfdx force:source:deploy -p force-app
-sfdx force:source:deploy -p orchestrator
+sf project deploy start -d force-app
+sf project deploy start -d orchestrator
 
 # Assign user the permset
-sfdx force:user:permset:assign -n Recruiting_Manager
+sf org assign permset -n Recruiting_Manager
 
 # Set the default password.
-sfdx shane:user:password:set -g User -l User -p salesforce1
+sf demoutil user password set -p salesforce1 -g User -l User
 
-sfdx force:apex:execute -f scripts/apex/createData.apex
-sfdx force:apex:execute -f scripts/apex/assignPermSet.apex
+sf apex run -f scripts/apex/createData.apex
+sf apex run -f scripts/apex/assignPermSet.apex
 
 # Open the org.
-sfdx force:org:open
+sf org open
